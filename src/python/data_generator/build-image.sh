2b3c95b6612eb35818c0a5840225189d773c54c9
Alexandre Gattiker <algattik@Gold.local>
2019-07-30 13:17:10 +0200

    Update build-image.sh

--- src/python/data_generator/build-image.sh
+++ src/python/data_generator/build-image.sh
@@ -6,6 +6,7 @@ set -euo pipefail
 RESOURCE_GROUP=${RESOURCE_GROUP-${PREFIX}}
 CONTAINER_REGISTRY=${CONTAINER_REGISTRY-${PREFIX}acr}
 PYTHON_PACKAGE_REPO=${PYTHON_PACKAGE_REPO}
+IMAGE_TAG=${BUILD_BUILDID:-latest}
 
 echo "checking container registry exists..."
 if ! az acr show -g $RESOURCE_GROUP -n $CONTAINER_REGISTRY ; then
@@ -16,7 +17,7 @@ fi
 
 echo "building generator container..."
 
-az acr build --registry $CONTAINER_REGISTRY --image generator:latest \
+az acr build --registry $CONTAINER_REGISTRY --image generator:$IMAGE_TAG \
   --secret-build-arg IMAGE_SERVER=$PYTHON_PACKAGE_REPO \
   . \
   -o tsv

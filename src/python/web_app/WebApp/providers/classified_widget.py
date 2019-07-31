7a6946b649447bf5a0ae60f45266707fa6f63ef2
David Crook <dacrook@microsoft.com>
2019-07-31 10:02:53 -0400

    modified widget provider and tests as well as fixed the docker file.

--- src/python/web_app/WebApp/providers/classified_widget.py
+++ src/python/web_app/WebApp/providers/classified_widget.py
@@ -3,10 +3,9 @@ Author: David Crook
 Copyright: Microsoft Corporation 2019
 """
 from ai_acc_quality.data_models.widget import Widget, Widget_Classification
+from typing import List
 
-def get_widget(db_cnxn, serial_number : str = None, db_id : str = None):
-    cursor = db_cnxn.cursor()
-    row = cursor.execute("select * from dbo.classified_widgets").fetchone()
+def widget_from_row(row) -> Widget:
     w_class = Widget_Classification()
     w_class.classified_time = row.classified_time
     w_class.is_good = row.is_good
@@ -21,3 +20,19 @@ def get_widget(db_cnxn, serial_number : str = None, db_id : str = None):
     wid.line_id = row.line_id
     wid.factory_id = row.factory_id
     return wid
+
+
+def get_widget(db_cnxn, serial_number : str = None, db_id : str = None) -> Widget:
+    cursor = db_cnxn.cursor()
+    if(serial_number is not None):
+        row = cursor.execute("select * from dbo.classified_widgets where serial_number == {}".format(serial_number)).fetchone()
+    elif(db_id is not None):
+        row = cursor.execute("select * from dbo.classified_widgets where id == {}".format(db_id)).fetchone()
+    return widget_from_row(row)
+
+def get_all_widgets(db_cnxn) -> List[Widget]:
+    cursor = db_cnxn.cursor()
+    widgets = []
+    for row in cursor.execute("select * from dbo.classified_widgets"):
+        widgets.append(widget_from_row(row))
+    return widgets

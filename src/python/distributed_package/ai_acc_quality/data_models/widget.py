6136758b7a1cc697c8d9a71fa55a6d9428665037
David Crook <dacrook@microsoft.com>
2019-07-31 11:37:20 -0400

    fixed bug with nones in object

--- src/python/distributed_package/ai_acc_quality/data_models/widget.py
+++ src/python/distributed_package/ai_acc_quality/data_models/widget.py
@@ -62,12 +62,14 @@ class Widget(Base_Model):
     def to_dict(self) -> Tuple[Result, Dict]:
         d = {}
         d["serial_number"] = self.serial_number
-        telies = []
-        for tel in self.telemetry:
-            _, t_dict = tel.to_dict()
-            telies.append(t_dict)
-        d["telemetry"] = telies
-        _, d["classification"] = self.classification.to_dict()
+        if(self.telemetry is not None):
+            telies = []
+            for tel in self.telemetry:
+                _, t_dict = tel.to_dict()
+                telies.append(t_dict)
+            d["telemetry"] = telies
+        if(self.classification is not None):
+            _, d["classification"] = self.classification.to_dict()
         return Result(True), d
 
     def to_json(self) -> str:

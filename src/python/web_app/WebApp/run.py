50ca18e560463e75750ec4a723bc6d4a1e133997
David Crook <dacrook@microsoft.com>
2019-08-02 12:54:55 -0400

    initial socket io sample in and working

--- src/python/web_app/WebApp/run.py
+++ src/python/web_app/WebApp/run.py
@@ -5,7 +5,15 @@ Copyright: Microsoft Corporation 2019
 
 from webapp.app import create_app
 from flask import render_template
+from flask_socketio import SocketIO
 
 app = create_app()
 
-app.run(host='0.0.0.0', port=80)
+socketio = SocketIO(app)
+
+@socketio.on('my event')
+def handle_message(message):
+    print('received message: ' + str(message))
+
+socketio.run(app, host="0.0.0.0", port=80)
+#app.run(host='0.0.0.0', port=80)

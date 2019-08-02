a8b6e13e7cbdbb0f96c24127551dce22df78f8ab
Alexandre Gattiker <algattik@Gold.local>
2019-08-02 10:08:08 +0200

    Revert "Revert "Support empty telemetry list""
    This reverts commit e0d03ffa053c59cbcf990ffdb0b09ca9d39ba974.

--- src/python/processing_function/ProcessTelemetry/__init__.py
+++ src/python/processing_function/ProcessTelemetry/__init__.py
@@ -3,9 +3,16 @@ import logging
 import azure.functions as func
 import random
 from datetime import datetime
+import pypyodbc
+import os
+from ProcessTelemetry.WidgetDAO import WidgetDAO
+import uuid
 
 from ai_acc_quality.data_models.telemetry import Telemetry
 from ai_acc_quality.data_models.widget import Widget, Widget_Classification
+
+def connectODBC():
+    return pypyodbc.connect(os.environ['SqlDatabaseConnectionString'])
 
 def main(event: func.EventHubEvent):
 
@@ -13,11 +20,18 @@ def main(event: func.EventHubEvent):
 
     c = Widget_Classification()
     c.classified_time = datetime.utcnow()
-    c.mean = random.randrange(1, 100)
-    c.std = random.randrange(1, 2)
-    c.std_dist = random.randrange(1, 3)
-    c.threshold = random.randrange(1, 100)
+    c.mean = random.randrange(0.0, 100.0)
+    c.std = random.randrange(0.0, 2.0)
+    c.std_dist = random.randrange(0.0, 3.0)
+    c.threshold = random.randrange(0.0, 100.0)
     w.classification = c
+
+    (result, good) = c.is_good()
+    assert result.success
+    if not good:
+        dao = WidgetDAO(connectODBC)
+        dao.persistWidget(w, uuid.uuid4().hex)
+        dao.disconnect()
 
     result = w.to_json()
 
---
+++ src/python/processing_function/tests/test_WidgetDAO.py
@@ -0,0 +1,52 @@
+"""
+Author: Alexandre Gattiker
+Handle: https://github.com/algattik
+"""
+import json
+import os
+from ProcessTelemetry.WidgetDAO import WidgetDAO
+from unittest.mock import Mock, ANY
+from datetime import datetime
+import pypyodbc
+
+from ai_acc_quality.data_models.telemetry import Telemetry
+from ai_acc_quality.data_models.widget import Widget, Widget_Classification
+
+class TestWidgetDAO(object):
+    """
+    Test Suite against event WidgetDAO
+    """
+
+    from unittest.mock import patch
+    @patch('pypyodbc.Connection')
+    def test_persist(self, mockConnection: pypyodbc.Connection):
+        """
+        Tests to ensure the generator posts events to event hub
+        """
+
+        dao = WidgetDAO(lambda : mockConnection)
+        w = generate_widget()
+        dao.persistWidget(w, "myid")
+
+        c = w.classification
+        mockConnection.cursor().execute.assert_called_once_with(ANY,
+            (w.serial_number, c.std_dist, c.std, c.mean,
+            c.threshold, c.is_good()[1], "myid",
+            w.factory_id, w.line_id, c.classified_time.isoformat(),))
+
+def generate_classification() -> Widget_Classification:
+    classification = Widget_Classification()
+    classification.classified_time = datetime.utcnow()
+    classification.mean = 1.0
+    classification.std = 2.0
+    classification.std_dist = 1.0
+    classification.threshold = 0.5
+    return classification
+
+def generate_widget() -> Widget:
+    w = Widget()
+    w.serial_number = "devserial1"
+    w.factory_id = "kitty hawk"
+    w.line_id = "1"
+    w.classification = generate_classification()
+    return w

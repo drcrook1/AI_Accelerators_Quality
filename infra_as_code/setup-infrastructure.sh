379059219f4c51c5f308bfe4b1d9f528bafdc706
Alexandre Gattiker <algattik@Gold.local>
2019-08-08 20:00:25 +0200

    Update setup-infrastructure.sh

--- infra_as_code/setup-infrastructure.sh
+++ infra_as_code/setup-infrastructure.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
+bash create-container-registry.sh
 bash create-storage-account.sh
 bash create-event-hub.sh
 bash create-sql-database.sh
 bash create-web-app.sh
 bash create-scoring-function.sh
-bash create-container-registry.sh

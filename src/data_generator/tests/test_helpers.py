9ae87b155637e8588ce81a7276001d7b96c6642b
David Crook <dacrook@microsoft.com>
2019-08-12 15:55:32 -0400

    fixed unit test

--- src/data_generator/tests/test_helpers.py
+++ src/data_generator/tests/test_helpers.py
@@ -14,12 +14,13 @@ class TestHelpers(object):
         """
         Test to ensure we can get a widget
         """
-        hub = helpers.get_event_hub()
+        hub, sender = helpers.get_event_hub()
+        hub.run()
         widgets = []
         widgets.append(helpers.generate_widget(is_anomaly=True))
         for i in range(0,10):
             widgets.append(helpers.generate_widget())
-        res = helpers.push_to_event_hub(hub, widgets)
+        res = helpers.push_to_event_hub(sender, widgets)
         assert(res is True)
     
     

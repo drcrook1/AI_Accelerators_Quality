e870e9aeeff7f4be0556335c481d5fc4010ea287
David Crook <dacrook@microsoft.com>
2019-08-12 15:48:39 -0400

    simplified entire solution to be a simple containerized while loop that puts random data on an event hub

---
+++ src/data_generator/tests/test_helpers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: David Crook
+@Author-Email: DaCrook@Microsoft.com
+"""
+import datagen.helpers as helpers
+
+class TestHelpers(object):
+    """
+    Test Suite against widgets provider
+    """
+
+    def test_push_msg(self):
+        """
+        Test to ensure we can get a widget
+        """
+        hub = helpers.get_event_hub()
+        widgets = []
+        widgets.append(helpers.generate_widget(is_anomaly=True))
+        for i in range(0,10):
+            widgets.append(helpers.generate_widget())
+        res = helpers.push_to_event_hub(hub, widgets)
+        assert(res is True)
+    
+    

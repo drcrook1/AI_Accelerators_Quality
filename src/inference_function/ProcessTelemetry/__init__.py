8174ab271ebb13e014cbc6bb8ec8e30ebeeae2b6
David Crook <dacrook@microsoft.com>
2019-09-04 16:37:48 -0400

    attempting ot add directory of init file to path in init file

--- src/inference_function/ProcessTelemetry/__init__.py
+++ src/inference_function/ProcessTelemetry/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 from .run import run
 import sys
 import os
-sys.path.append(os.path.abspath(""))
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 def main(event: func.EventHubEvent):
     event_json = event.get_body().decode('utf-8')
